23f3883a58468578c6acbc73080f021441764eec
sbminimalistics <p.arvydas@gmail.com>
2023-07-30 21:50:19 +0300

    append example with 100KB,  delimiter and timestamp bytes write steps

--- example.js
+++ example.js
@@ -14,12 +14,23 @@ const delimiterLength = delimiter.length;
 // synchronously read test data files
 const data0 = fs.readFileSync(path.resolve(__dirname, srcPath, "10KB.data"));
 const data1 = fs.readFileSync(path.resolve(__dirname, srcPath, "1B.data"));
+const data2 = fs.createReadStream(path.resolve(__dirname, srcPath, "100KB.data"));
 
 // define output data file path (will be created if does not exist)
 const fileDBPath = path.resolve(__dirname, outPath, "out.data");
 
 // create file db instance and add event listeners
 const fileDB = new FileDB(fileDBPath);
+
+const DELIMITER = Buffer.from([127, 127, 127, 127]);
+
+const getTimeBytes = () => {
+    return Buffer.from((new Date().getTime()).toString(16).padStart(12, "0"), "hex");
+}
+
+const getLengthBytes = (data) => {
+    return Buffer.from((data.length).toString(16).padStart(8, "0"), "hex");
+}
 /* fileDB.on(Event.SIZE, (size) => {
     console.log("current db file size:", size, "bytes");
 }) */
@@ -32,6 +43,9 @@ fileDB.read(res => console.log("read cb from app, res.length:", res.length)).app
 // fileDB.read().then(res => console.log("read.then from app, res.length:", res.length));
 
 fileDB
+.append(DELIMITER)
+.append(getTimeBytes())
+.append(getLengthBytes(data1))
 .append(data1)
 .then(res => {
     console.log("1st append res:", res);
@@ -39,14 +53,20 @@ fileDB
 .read((data) => {
     console.log("read cb data:", data);
 }, {start: 0, end: 3}) // just the first three bytes;
+.append(DELIMITER)
+.append(getTimeBytes())
+.append(getLengthBytes(data0))
 .append(data0)
+.append(DELIMITER)
+.append(getLengthBytes(Buffer.from([100])))
+.append(data2)
 .then(res => {
     console.log("2nd append res:", res);
     fileDB.close();
     delete fileDB;
 }).catch(err => {
     console.log("catch err:", err);
-});
+}).then(res => console.log("eof example"));
 
 
 /* fileDB.on(Event.SIZE, (size) => {
